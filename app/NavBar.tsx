ad3930911e8931136f8c597db5f7bcfdf74ed0d8
Samir Mishra <samirmishra.a1@gmail.com>
2023-10-20 02:39:14 +0530

    feat: style the active link in the navbar
    style the active link to be more darker & noticeable in the navbar
    component.
    Use classnames library to efficiently write conditional tailwind styles.

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,16 @@
-import Link from 'next/link'
+'use client'
+
 import React from 'react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+import classNames from 'classnames'
+
 import { VscDebugAlt } from 'react-icons/vsc'
 
 const NavBar = () => {
+    const currentPath = usePathname()
+
     const links = [
         { label: 'Dashboard', href: '/' },
         { label: 'Issues', href: '/issues' }
@@ -17,7 +25,11 @@ const NavBar = () => {
                 {links.map((link) => (
                     <Link
                         key={link.href}
-                        className=" text-zinc-500 hover:text-zinc-800 transition-colors"
+                        className={classNames({
+                            ' text-zinc-900': link.href === currentPath,
+                            'text-zinc-500': link.href !== currentPath,
+                            'hover:text-zinc-800 transition-colors': true
+                        })}
                         href={link.href}>
                         {link.label}
                     </Link>
