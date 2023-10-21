ba9027a6aaa424cc5ea2be39b2895d019c3916cd
Samir Mishra <samirmishra.a1@gmail.com>
2023-10-22 03:17:33 +0530

    refactor: extract the error message component
    Extract the error message component to avoid repeating code and clean interface.

---
+++ app/components/ErrorMessage.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { PropsWithChildren } from 'react'
+import { Text } from '@radix-ui/themes'
+
+const ErrorMessage = ({ children }: PropsWithChildren) => {
+    if (!children) return null
+
+    return (
+        <Text color="red" as="p">
+            {children}
+        </Text>
+    )
+}
+
+export default ErrorMessage
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -9,6 +9,7 @@ import { useRouter } from 'next/navigation'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { createIssueSchema } from '@/app/validationSchemas'
 import { z } from 'zod'
+import ErrorMessage from '@/app/components/ErrorMessage'
 
 type IssueForm = z.infer<typeof createIssueSchema>
 
@@ -45,21 +46,13 @@ const NewIssuePage = () => {
                 <TextField.Root>
                     <TextField.Input placeholder="Enter Title..." {...register('title')} />
                 </TextField.Root>
-                {errors.title && (
-                    <Text color="red" as="p">
-                        {errors.title.message}
-                    </Text>
-                )}
+                <ErrorMessage>{errors.title?.message}</ErrorMessage>
                 <Controller
                     name="description"
                     control={control}
                     render={({ field }) => <SimpleMDE placeholder=" Description of the issue..." {...field} />}
                 />
-                {errors.description && (
-                    <Text color="red" as="p">
-                        {errors.description.message}
-                    </Text>
-                )}
+                <ErrorMessage>{errors.description?.message}</ErrorMessage>
                 <Button>Submit New Issue</Button>
             </form>
         </div>
