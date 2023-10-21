3130219fabf7749117e8ed4009173d49757704a1
Samir Mishra <samirmishra.a1@gmail.com>
2023-10-22 02:49:11 +0530

    feat: handle errors on form submission
    Handle errors on form submission of creating issues, show a generic
    display error to user when request fails.

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import { Button, TextField } from '@radix-ui/themes'
+import { useState } from 'react'
+import { Button, TextField, Callout } from '@radix-ui/themes'
 import SimpleMDE from 'react-simplemde-editor'
 import { Controller, useForm } from 'react-hook-form'
 import 'easymde/dist/easymde.min.css'
@@ -13,26 +14,40 @@ interface IssueForm {
 
 const NewIssuePage = () => {
     const router = useRouter()
+
     const { register, control, handleSubmit } = useForm<IssueForm>()
+    const [error, setError] = useState('')
 
     return (
-        <form
-            autoComplete="off"
-            className=" max-w-xl space-y-3"
-            onSubmit={handleSubmit(async (data) => {
-                await fetch('/api/issues', { body: JSON.stringify(data), method: 'POST' })
-                router.push('/issues')
-            })}>
-            <TextField.Root>
-                <TextField.Input placeholder="Enter Title..." {...register('title')} />
-            </TextField.Root>
-            <Controller
-                name="description"
-                control={control}
-                render={({ field }) => <SimpleMDE placeholder=" Description of the issue..." {...field} />}
-            />
-            <Button>Submit New Issue</Button>
-        </form>
+        <div className=" max-w-xl">
+            {error && (
+                <Callout.Root color="red" className=" mb-5">
+                    <Callout.Text>{error}</Callout.Text>
+                </Callout.Root>
+            )}
+            <form
+                autoComplete="off"
+                className=" space-y-3"
+                onSubmit={handleSubmit(async (data) => {
+                    try {
+                        const response = await fetch('/api/issues', { body: JSON.stringify(data), method: 'POST' })
+                        if (!response.ok) throw new Error()
+                        router.push('/issues')
+                    } catch (error) {
+                        setError('An unexpected error occurred!')
+                    }
+                })}>
+                <TextField.Root>
+                    <TextField.Input placeholder="Enter Title..." {...register('title')} />
+                </TextField.Root>
+                <Controller
+                    name="description"
+                    control={control}
+                    render={({ field }) => <SimpleMDE placeholder=" Description of the issue..." {...field} />}
+                />
+                <Button>Submit New Issue</Button>
+            </form>
+        </div>
     )
 }
 
