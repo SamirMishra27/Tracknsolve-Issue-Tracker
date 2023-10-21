4c1b8c7cadd0f6509f55f30c55a3685a50a18bc1
Samir Mishra <samirmishra.a1@gmail.com>
2023-10-22 02:18:19 +0530

    feat: handle form submission for creating new issues
    Use react hook form to accept user input and on form submission handle
    it to be sent to the api to create a new record and then direct user to
    /issues page.

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,17 +2,37 @@
 
 import { Button, TextField } from '@radix-ui/themes'
 import SimpleMDE from 'react-simplemde-editor'
+import { Controller, useForm } from 'react-hook-form'
 import 'easymde/dist/easymde.min.css'
+import { useRouter } from 'next/navigation'
+
+interface IssueForm {
+    title: string
+    description: string
+}
 
 const NewIssuePage = () => {
+    const router = useRouter()
+    const { register, control, handleSubmit } = useForm<IssueForm>()
+
     return (
-        <div className=" max-w-xl space-y-3">
+        <form
+            autoComplete="off"
+            className=" max-w-xl space-y-3"
+            onSubmit={handleSubmit(async (data) => {
+                await fetch('/api/issues', { body: JSON.stringify(data), method: 'POST' })
+                router.push('/issues')
+            })}>
             <TextField.Root>
-                <TextField.Input placeholder="Enter Title..." />
+                <TextField.Input placeholder="Enter Title..." {...register('title')} />
             </TextField.Root>
-            <SimpleMDE placeholder=" Description of the issue..." />
+            <Controller
+                name="description"
+                control={control}
+                render={({ field }) => <SimpleMDE placeholder=" Description of the issue..." {...field} />}
+            />
             <Button>Submit New Issue</Button>
-        </div>
+        </form>
     )
 }
 
