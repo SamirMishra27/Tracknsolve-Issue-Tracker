74edcd37147c768a2a6d2ced1d61b3806d130e59
Samir Mishra <samirmishra.a1@gmail.com>
2023-10-24 00:42:10 +0530

    feat: add loading skeletons for issues page
    Add loading skeletons before issues are fully loaded to enhance user
    experience and reduce perceived loading time.

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,12 +1,10 @@
-import React from 'react'
 import { Table } from '@radix-ui/themes'
-
-import prisma from '@/prisma/client'
-import IssueStatusBadge from '../components/IssueStatusBadge'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 import IssueActions from './IssueActions'
 
-const IssuesPage = async () => {
-    const issues = await prisma.issue.findMany()
+const LoadingIssuesPage = () => {
+    const issues = [1, 2, 3, 4, 5]
 
     return (
         <div>
@@ -21,17 +19,19 @@ const IssuesPage = async () => {
                 </Table.Header>
                 <Table.Body>
                     {issues.map((issue) => (
-                        <Table.Row key={issue.id}>
+                        <Table.Row key={issue}>
                             <Table.Cell>
-                                {issue.title}
+                                <Skeleton />
                                 <div className=" block md:hidden">
-                                    <IssueStatusBadge status={issue.status} />
+                                    <Skeleton />
                                 </div>
                             </Table.Cell>
                             <Table.Cell className=" hidden md:table-cell">
-                                <IssueStatusBadge status={issue.status} />
+                                <Skeleton />
                             </Table.Cell>
-                            <Table.Cell className=" hidden md:table-cell">{issue.createdAt.toDateString()}</Table.Cell>
+                            <Table.Cell className=" hidden md:table-cell">
+                                <Skeleton />
+                            </Table.Cell>
                         </Table.Row>
                     ))}
                 </Table.Body>
@@ -40,4 +40,4 @@ const IssuesPage = async () => {
     )
 }
 
-export default IssuesPage
+export default LoadingIssuesPage
