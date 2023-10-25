17e63aec853de874d74209bc514ebbafd15485fe
Samir Mishra <samirmishra.a1@gmail.com>
2023-10-25 06:42:33 +0530

    feat: add styled links component to issues page
    Add styling to issue title links on issues page. Build a styled link component.

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -4,7 +4,7 @@ import { Table } from '@radix-ui/themes'
 import prisma from '@/prisma/client'
 import IssueStatusBadge from '../components/IssueStatusBadge'
 import IssueActions from './IssueActions'
-import Link from 'next/link'
+import Link from '../components/Link'
 
 const IssuesPage = async () => {
     const issues = await prisma.issue.findMany()
