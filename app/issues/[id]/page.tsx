5583829539fa15a6a163138a2c022ae177557bdb
Samir Mishra <samirmishra.a1@gmail.com>
2023-10-26 02:45:04 +0530

    feat: add the edit button to edit issues
    Add the edit button to edit issues on issues view page. Make it
    responsive as well.

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,9 +1,11 @@
-import { Heading, Text, Flex, Card } from '@radix-ui/themes'
+import { Heading, Text, Flex, Card, Grid, Box, Button } from '@radix-ui/themes'
+import { Pencil2Icon } from '@radix-ui/react-icons'
 import { notFound } from 'next/navigation'
 
 import prisma from '@/prisma/client'
 import IssueStatusBadge from '@/app/components/IssueStatusBadge'
 import ReactMarkdown from 'react-markdown'
+import Link from 'next/link'
 
 interface Props {
     params: { id: string }
@@ -17,16 +19,24 @@ const page = async ({ params }: Props) => {
     if (!issue) notFound()
 
     return (
-        <div>
-            <Heading>{issue.title}</Heading>
-            <Flex gap="3" my="2">
-                <IssueStatusBadge status={issue.status} />
-                <Text>{issue.createdAt.toDateString()}</Text>
-            </Flex>
-            <Card className=" prose" mt="4">
-                <ReactMarkdown>{issue.description}</ReactMarkdown>
-            </Card>
-        </div>
+        <Grid columns={{ initial: '1', md: '2' }} gap="5">
+            <Box>
+                <Heading>{issue.title}</Heading>
+                <Flex gap="3" my="2">
+                    <IssueStatusBadge status={issue.status} />
+                    <Text>{issue.createdAt.toDateString()}</Text>
+                </Flex>
+                <Card className=" prose" mt="4">
+                    <ReactMarkdown>{issue.description}</ReactMarkdown>
+                </Card>
+            </Box>
+            <Box>
+                <Button>
+                    <Pencil2Icon />
+                    <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
+                </Button>
+            </Box>
+        </Grid>
     )
 }
 
