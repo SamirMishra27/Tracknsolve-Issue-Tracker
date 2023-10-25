b27444ed3b9b880e1673fcc36912b7edde2b534a
Samir Mishra <samirmishra.a1@gmail.com>
2023-10-26 03:00:02 +0530

    refactor: apply the single responsibility principle in issue view page
    Abstract components into their own files to make code more maintainable
    and apply the single responsibility principle. (or SRP)

---
+++ app/issues/[id]/IssueDetails.tsx
@@ -0,0 +1,22 @@
+import { Heading, Text, Flex, Card } from '@radix-ui/themes'
+import { Issue } from '@prisma/client'
+
+import IssueStatusBadge from '@/app/components/IssueStatusBadge'
+import ReactMarkdown from 'react-markdown'
+
+const IssueDetails = ({ issue }: { issue: Issue }) => {
+    return (
+        <>
+            <Heading>{issue.title}</Heading>
+            <Flex gap="3" my="2">
+                <IssueStatusBadge status={issue.status} />
+                <Text>{issue.createdAt.toDateString()}</Text>
+            </Flex>
+            <Card className=" prose" mt="4">
+                <ReactMarkdown>{issue.description}</ReactMarkdown>
+            </Card>
+        </>
+    )
+}
+
+export default IssueDetails
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,11 +1,9 @@
-import { Heading, Text, Flex, Card, Grid, Box, Button } from '@radix-ui/themes'
-import { Pencil2Icon } from '@radix-ui/react-icons'
+import { Grid, Box } from '@radix-ui/themes'
 import { notFound } from 'next/navigation'
 
 import prisma from '@/prisma/client'
-import IssueStatusBadge from '@/app/components/IssueStatusBadge'
-import ReactMarkdown from 'react-markdown'
-import Link from 'next/link'
+import EditIssueButton from './EditIssueButton'
+import IssueDetails from './IssueDetails'
 
 interface Props {
     params: { id: string }
@@ -21,20 +19,10 @@ const page = async ({ params }: Props) => {
     return (
         <Grid columns={{ initial: '1', md: '2' }} gap="5">
             <Box>
-                <Heading>{issue.title}</Heading>
-                <Flex gap="3" my="2">
-                    <IssueStatusBadge status={issue.status} />
-                    <Text>{issue.createdAt.toDateString()}</Text>
-                </Flex>
-                <Card className=" prose" mt="4">
-                    <ReactMarkdown>{issue.description}</ReactMarkdown>
-                </Card>
+                <IssueDetails issue={issue} />
             </Box>
             <Box>
-                <Button>
-                    <Pencil2Icon />
-                    <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
-                </Button>
+                <EditIssueButton issueId={issue.id} />
             </Box>
         </Grid>
     )
