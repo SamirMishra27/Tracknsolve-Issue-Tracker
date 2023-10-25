1082a1a42f06244b4ad080305e5405cca7613aee
Samir Mishra <samirmishra.a1@gmail.com>
2023-10-25 06:04:09 +0530

    feat: show issue details on issues/id page
    Create a new page to show specific issue in detail & display all
    important details of it. On route /issues/[id].

---
+++ app/issues/[id]/page.tsx
@@ -0,0 +1,25 @@
+import { notFound } from 'next/navigation'
+import prisma from '@/prisma/client'
+
+interface Props {
+    params: { id: string }
+}
+
+const page = async ({ params }: Props) => {
+    const issue = await prisma.issue.findUnique({
+        where: { id: parseInt(params.id) }
+    })
+
+    if (!issue) notFound()
+
+    return (
+        <div>
+            <p>{issue.title}</p>
+            <p>{issue.description}</p>
+            <p>{issue.status}</p>
+            <p>{issue.createdAt.toDateString()}</p>
+        </div>
+    )
+}
+
+export default page
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -4,6 +4,7 @@ import { Table } from '@radix-ui/themes'
 import prisma from '@/prisma/client'
 import IssueStatusBadge from '../components/IssueStatusBadge'
 import IssueActions from './IssueActions'
+import Link from 'next/link'
 
 const IssuesPage = async () => {
     const issues = await prisma.issue.findMany()
@@ -23,7 +24,7 @@ const IssuesPage = async () => {
                     {issues.map((issue) => (
                         <Table.Row key={issue.id}>
                             <Table.Cell>
-                                {issue.title}
+                                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
                                 <div className=" block md:hidden">
                                     <IssueStatusBadge status={issue.status} />
                                 </div>
