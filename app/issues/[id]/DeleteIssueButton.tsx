3323753c76350e64b45cfc6b06a75a08d1d08547
Samir Mishra <samirmishra.a1@gmail.com>
2023-10-27 01:55:40 +0530

    feat: improve the user experience when deleting an issue
    Improve the user experience when user deletes an issue by showing a
    proper loading spinner and keep button disabled to indicate that
    deletion is in progress.

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,21 +1,25 @@
 'use client'
 
-import { AlertDialog, Button, Flex } from '@radix-ui/themes'
-import { useRouter } from 'next/navigation'
 import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { AlertDialog, Button, Flex } from '@radix-ui/themes'
+import { Spinner } from '@/app/components'
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
     const router = useRouter()
     const [error, setError] = useState(false)
+    const [isDeleting, setDeleting] = useState(false)
 
     const deleteIssue = async () => {
         try {
+            setDeleting(true)
             const response = await fetch(`/api/issues/${issueId}`, { method: 'DELETE' })
             if (!response.ok) throw new Error()
 
             router.push('/issues')
             router.refresh()
         } catch (error) {
+            setDeleting(false)
             setError(true)
         }
     }
@@ -24,7 +28,10 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
         <>
             <AlertDialog.Root>
                 <AlertDialog.Trigger>
-                    <Button color="red">Delete Issue</Button>
+                    <Button color="red" disabled={isDeleting}>
+                        Delete Issue
+                        {isDeleting && <Spinner />}
+                    </Button>
                 </AlertDialog.Trigger>
                 <AlertDialog.Content>
                     <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
