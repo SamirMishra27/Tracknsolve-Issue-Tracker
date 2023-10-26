479156ae9f355aa60ee85547faaa8b3c827b772f
Samir Mishra <samirmishra.a1@gmail.com>
2023-10-27 01:17:47 +0530

    feat: build an api for deleting issues
    Add an api route in next's api directory to delete issues by their id.

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -18,3 +18,13 @@ export async function PATCH(request: NextRequest, { params }: { params: { id: st
 
     return NextResponse.json(updatedIssue)
 }
+
+export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
+    const issue = await prisma.issue.findUnique({ where: { id: parseInt(params.id) } })
+
+    if (!issue) return NextResponse.json({ error: 'Invalid issue' }, { status: 404 })
+
+    await prisma.issue.delete({ where: { id: issue.id } })
+
+    return NextResponse.json({})
+}
