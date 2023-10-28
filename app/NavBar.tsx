1cf29f5c0852abc74d1886e8e5ec563506a75729
Samir Mishra <samirmishra.a1@gmail.com>
2023-10-28 17:24:57 +0530

    feat: add the login & logout links
    The the login & logout button links in the dashboard navbar & setup
    session provider of next-auth.

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,15 +1,16 @@
 'use client'
 
-import React from 'react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import { useSession } from 'next-auth/react'
 
 import classNames from 'classnames'
-
 import { VscDebugAlt } from 'react-icons/vsc'
+import { Box } from '@radix-ui/themes'
 
 const NavBar = () => {
     const currentPath = usePathname()
+    const { status, data: session } = useSession()
 
     const links = [
         { label: 'Dashboard', href: '/' },
@@ -23,18 +24,23 @@ const NavBar = () => {
             </Link>
             <ul className=" flex space-x-6">
                 {links.map((link) => (
-                    <Link
-                        key={link.href}
-                        className={classNames({
-                            ' text-zinc-900': link.href === currentPath,
-                            'text-zinc-500': link.href !== currentPath,
-                            'hover:text-zinc-800 transition-colors': true
-                        })}
-                        href={link.href}>
-                        {link.label}
-                    </Link>
+                    <li key={link.href}>
+                        <Link
+                            className={classNames({
+                                ' text-zinc-900': link.href === currentPath,
+                                'text-zinc-500': link.href !== currentPath,
+                                'hover:text-zinc-800 transition-colors': true
+                            })}
+                            href={link.href}>
+                            {link.label}
+                        </Link>
+                    </li>
                 ))}
             </ul>
+            <Box>
+                {status === 'authenticated' && <Link href="/api/auth/signin">Log out</Link>}
+                {status === 'unauthenticated' && <Link href="/api/auth/signout">Login</Link>}
+            </Box>
         </nav>
     )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import NavBar from './NavBar'
+import AuthProvider from './auth/AuthProvider'
 import { Container, Theme } from '@radix-ui/themes'
 
 const inter = Inter({ subsets: ['latin'], display: 'swap', variable: '--font-inter' })
@@ -17,12 +18,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html lang="en">
             <body className={inter.variable}>
-                <Theme accentColor="violet">
-                    <NavBar />
-                    <main className=" p-5">
-                        <Container>{children}</Container>
-                    </main>
-                </Theme>
+                <AuthProvider>
+                    <Theme accentColor="violet">
+                        <NavBar />
+                        <main className=" p-5">
+                            <Container>{children}</Container>
+                        </main>
+                    </Theme>
+                </AuthProvider>
             </body>
         </html>
     )
