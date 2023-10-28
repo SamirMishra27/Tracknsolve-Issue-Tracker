713738477d6668e77abee138abecd0267f43ad4d
Samir Mishra <samirmishra.a1@gmail.com>
2023-10-29 02:50:00 +0530

    feat: populate the assignee select component
    - Populate the assignee select component with real users from the
      database.
    - Build an api route /users to get all the users from the database.
      (ever signed in to the site).

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,16 +1,33 @@
 'use client'
 
+import { User } from '@prisma/client'
 import { Select } from '@radix-ui/themes'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const AssigneeSelect = () => {
+    const [users, setUsers] = useState<User[]>([])
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            const response = await fetch('/api/users', { method: 'GET' })
+            const users: User[] = await response.json()
+            setUsers(users)
+        }
+
+        fetchUsers()
+    }, [])
+
     return (
         <Select.Root>
             <Select.Trigger placeholder="Assign..." />
             <Select.Content>
                 <Select.Group>
                     <Select.Label>Suggestions</Select.Label>
-                    <Select.Item value="1">Samir Mishra</Select.Item>
+                    {users.map((user) => (
+                        <Select.Item value={user.id} key={user.id}>
+                            {user.name}
+                        </Select.Item>
+                    ))}
                 </Select.Group>
             </Select.Content>
         </Select.Root>
