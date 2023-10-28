6451c7c9c8c513b9f5855cc64c5d5818e96a3d6b
Samir Mishra <samirmishra.a1@gmail.com>
2023-10-29 02:34:32 +0530

    feat: build the assignee select component
    Build the assigneee select component which will be used to select &
    assign users.

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -7,6 +7,7 @@ import IssueDetails from './IssueDetails'
 import DeleteIssueButton from './DeleteIssueButton'
 import { getServerSession } from 'next-auth'
 import authOptions from '@/app/auth/authOptions'
+import AssigneeSelect from './AssigneeSelect'
 
 interface Props {
     params: { id: string }
@@ -29,6 +30,7 @@ const page = async ({ params }: Props) => {
             {session && (
                 <Box>
                     <Flex direction="column" gap="4">
+                        <AssigneeSelect />
                         <EditIssueButton issueId={issue.id} />
                         <DeleteIssueButton issueId={issue.id} />
                     </Flex>
