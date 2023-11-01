d83264ecb135fbe32c8b4cfe83f42dc94176883a
Samir Mishra <samirmishra.a1@gmail.com>
2023-11-01 17:13:42 +0530

    feat: build the filter component
    Build the issue filter component that will be used to filter issues by
    their status. (open, in progress, closed, etc)

---
+++ app/issues/list/IssueStatusFilter.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { Status } from '@prisma/client'
+import { Select } from '@radix-ui/themes'
+
+const issueStatus: { label: string; value?: Status }[] = [
+    { label: 'All' },
+    { label: 'Open', value: 'OPEN' },
+    { label: 'In Progress', value: 'IN_PROGRESS' },
+    { label: 'Closed', value: 'CLOSED' }
+]
+
+const IssueStatusFilter = () => {
+    return (
+        <Select.Root>
+            <Select.Trigger placeholder="Filter by status..." />
+            <Select.Content>
+                {issueStatus.map((status) => (
+                    <Select.Item key={status.value} value={status.value || ''}>
+                        {status.label}
+                    </Select.Item>
+                ))}
+            </Select.Content>
+        </Select.Root>
+    )
+}
+
+export default IssueStatusFilter
