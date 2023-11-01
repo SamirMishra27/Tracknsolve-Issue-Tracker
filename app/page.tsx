13c19be1c565543ff116c4d06c8720ce26402789
Samir Mishra <samirmishra.a1@gmail.com>
2023-11-02 05:24:03 +0530

    feat: build the latest issue component.
    Build the latest issue component & add it in the home page.
    (LatestIssue.tsx)

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
-import Pagination from './components/Pagination'
+import { LatestIssues } from './LatestIssues'
 
-export default function Home({ searchParams }: { searchParams: { page: string } }) {
-    return (
-        <>
-            <div>Hello World!</div>
-            <Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page || '1')} />
-        </>
-    )
+export default function Home() {
+    return <LatestIssues />
 }
