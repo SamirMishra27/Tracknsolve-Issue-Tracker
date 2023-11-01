911926c783772652f7075422895141a36b4f072b
Samir Mishra <samirmishra.a1@gmail.com>
2023-11-01 20:57:18 +0530

    feat: implement pagination logic
    Implement pagination logic using search query params.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import Pagination from './components/Pagination'
 
-export default function Home() {
+export default function Home({ searchParams }: { searchParams: { page: string } }) {
     return (
         <>
             <div>Hello World!</div>
-            <Pagination itemCount={100} pageSize={10} currentPage={1} />
+            <Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page || '1')} />
         </>
     )
 }
