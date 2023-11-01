7ed58b00b27de068f4e6b93b946b81dffd3f51f5
Samir Mishra <samirmishra.a1@gmail.com>
2023-11-01 18:23:06 +0530

    fix: filtering bugs in issues page
    Fix some issues with applying search parameters of filtering in issues page.

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -2,7 +2,7 @@
 
 import { Status } from '@prisma/client'
 import { Select } from '@radix-ui/themes'
-import { useRouter } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 
 const issueStatus: { label: string; value?: Status }[] = [
     { label: 'All' },
@@ -13,11 +13,18 @@ const issueStatus: { label: string; value?: Status }[] = [
 
 const IssueStatusFilter = () => {
     const router = useRouter()
+    const searchParams = useSearchParams()
 
     return (
         <Select.Root
+            defaultValue={searchParams.get('status')!}
             onValueChange={(status) => {
-                const query = status === 'ALL' ? '' : `?status=${status}`
+                const params = new URLSearchParams()
+
+                if (status) params.append('status', status)
+                if (searchParams.get('orderBy')) params.append('orderBy', searchParams.get('orderBy')!)
+
+                const query = params.size ? '?' + params.toString() : ''
                 router.push('/issues/list' + query)
             }}>
             <Select.Trigger placeholder="Filter by status..." />
