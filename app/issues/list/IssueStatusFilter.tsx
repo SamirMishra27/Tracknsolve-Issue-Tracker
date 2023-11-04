d73b7578d8d1b9516e4efb532ee341e60ce7bb51
Samir Mishra <samirmishra.a1@gmail.com>
2023-11-05 02:56:59 +0530

    fix issue status filter not working in some browsers
    Fix issue status filter not working in some browsers due to
    compatibility issues.
    Fixes issue #1

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -17,14 +17,14 @@ const IssueStatusFilter = () => {
 
     return (
         <Select.Root
-            defaultValue={searchParams.get('status')!}
+            defaultValue={searchParams.get('status') || ''}
             onValueChange={(status) => {
                 const params = new URLSearchParams()
 
                 if (status) params.append('status', status)
                 if (searchParams.get('orderBy')) params.append('orderBy', searchParams.get('orderBy')!)
 
-                const query = params.size ? '?' + params.toString() : ''
+                const query = Array.from(params).length ? '?' + params.toString() : ''
                 router.push('/issues/list' + query)
             }}>
             <Select.Trigger placeholder="Filter by status..." />
